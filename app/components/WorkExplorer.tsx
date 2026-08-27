bb02e8af21b2a71786c6f8fc6ecd1381d8a3b710
Jiajun Fu <fukajun2002@gmail.com>
2026-08-27 15:13:42 +0800

    Bath pain 01: trim to single unpredictability point

--- app/components/WorkExplorer.tsx
+++ app/components/WorkExplorer.tsx
@@ -309,7 +309,7 @@ function BathProjectDetail() {
             </p>
 
             <div className="bath-pain-list">
-              <div className="bath-pain-item"><b>01</b><span className="bath-pain-title">消耗不可预期</span><span className="bath-pain-desc">用户无法预判「完全洗干净需要多少道具」，也无法感知「拖动到何种程度会扣除一块香皂并增加清洁值」。</span></div>
+              <div className="bath-pain-item"><b>01</b><span className="bath-pain-title">消耗不可预期</span><span className="bath-pain-desc">用户无法预知「完全洗干净需要消耗多少道具」</span></div>
               <div className="bath-pain-item"><b>02</b><span className="bath-pain-title">消耗与清洁值难以同步感知</span><span className="bath-pain-desc">用户已建立点击消耗的认知，而拖拽是连续的，用户难以同时感知「扣了多少道具」和「涨了多少清洁值」。</span></div>
             </div>
 
